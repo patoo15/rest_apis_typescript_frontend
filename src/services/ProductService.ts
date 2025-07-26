69553f3120b08a7a8bb1ad18a627d73a0a140ed7
patoo15 <patriciotorres1992@hotmail.com>
2025-07-25 21:16:13 -0300

    first commit

---
+++ src/services/ProductService.ts
@@ -0,0 +1,96 @@
+import { safeParse } from "valibot";
+import { DraftProductSchema, Product, ProductSchema, ProductsSchema } from "../types";
+import axios from "axios";
+
+type ProductData = {
+  [k: string]: FormDataEntryValue;
+};
+
+export async function addProduct(data: ProductData) {
+  try {
+    const result = safeParse(DraftProductSchema, {
+      name: data.name,
+      price: +data.price,
+    });
+    if (result.success) {
+      const url = `${import.meta.env.VITE_API_URL}/api/products`;
+      await axios.post(url, {
+        name: result.output.name,
+        price: result.output.price,
+      });
+    } else {
+      throw new Error("Datos no válidos");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getProducts() {
+  try {
+    const url = `${import.meta.env.VITE_API_URL}/api/products`;
+    const { data } = await axios(url);
+    const result = safeParse(ProductsSchema, data.data);
+    if (result.success) {
+      return result.output;
+    } else {
+      throw new Error("Hubo un error...");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getProductById(id: Product["id"]) {
+  try {
+    const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
+    const { data } = await axios(url);
+    const result = safeParse(ProductSchema, data.data);
+    if (result.success) {
+      return result.output;
+    } else {
+      throw new Error("Hubo un error...");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function updateProduct(data: ProductData, id: Product["id"]) {
+  try {
+    const transformedData = {
+      id,
+      name: data.name,
+      price: Number(data.price),
+      availability: data.availability === "true",
+    };
+    const result = safeParse(ProductSchema, transformedData);
+
+    if (result.success) {
+      const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
+      await axios.put(url, result.output);
+    } else {
+      throw new Error("Datos no válidos para actualizar producto");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function deleteProduct(id: Product["id"]) {
+  try {
+    const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
+    await axios.delete(url);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function updateProductAvailability(id: Product["id"]) {
+  try {
+    const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
+    await axios.patch(url);
+  } catch (error) {
+    console.log(error);
+  }
+}
